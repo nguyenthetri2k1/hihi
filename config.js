a5c4ee37f544fdd7ba65722f1d9ac5342b805fd4
nguyenthetri2k1 <92871783+nguyenthetri2k1@users.noreply.github.com>
2021-10-20 22:28:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const nameGirl = 'Lê Thị Lương Huyên';
-const giftUrl = 'http://nodemy.vn';
+const nameGirl = 'Lương Huyên';
+const giftUrl = 'https://www.facebook.com/lethiluong.huyen.77';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'LTLH <3';
 const contentCard = 'Chúc Huyên 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Huyên trong hôm nay và cả những ngày sau. HIHI ^.^';
